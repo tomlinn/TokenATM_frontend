923309fbc297074d2dc5ede4aefccb52faa51d98
Christina0114 <chewnenjun57@live.cn>
2022-12-04 22:14:22 -0800

    translate 404 page

--- src/views/common/404.vue
+++ src/views/common/404.vue
@@ -3,9 +3,9 @@
     <div class="site-content__wrapper">
       <div class="site-content">
         <h2 class="not-found-title">400</h2>
-        <p class="not-found-desc">抱歉！您访问的页面<em>失联</em>啦 ...</p>
-        <el-button @click="$router.go(-1)">返回上一页</el-button>
-        <el-button type="primary" class="not-found-btn-gohome" @click="$router.push({ name: 'home' })">进入首页</el-button>
+        <p class="not-found-desc">sorry <em>lost connection</em> ...</p>
+        <el-button @click="$router.go(-1)">back to last page</el-button>
+        <el-button type="primary" class="not-found-btn-gohome" @click="$router.push({ name: 'home' })">enter home page</el-button>
       </div>
     </div>
   </div>
