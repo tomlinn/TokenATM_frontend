f78e2e35db63b52a85e59a9500b56075c9912d38
Ching-Yang Lin <chingyang886@gmail.com>
2023-02-08 11:59:42 -0800

    add undo feature

--- src/views/modules/token/pastResubmission.vue
+++ src/views/modules/token/pastResubmission.vue
@@ -34,6 +34,12 @@
         label="Token Required"
         width="150">
       </el-table-column>
+      <el-table-column header-align="center" align="left" width="150" label="Undo">
+            <template slot-scope="scope">
+            <el-button  type="text" size="small" @click="cancel(scope.row, scope.$index)"
+              >Undo resubmission</el-button>
+            </template>
+          </el-table-column>
     </el-table>
   </div>
   </template>
@@ -42,7 +48,8 @@
     export default {
       data() {
         return {
-          tableData: []
+          tableData: [],
+          userId: this.$store.state.user.studentID,
         }
       },
       filters: {
@@ -55,6 +62,51 @@
 		    }
 		},
       methods: {
+      cancel(data, index) {
+          this.$confirm('Do you want a resubmission?', 'Alert', {
+          confirmButtonText: 'Yes',
+          cancelButtonText: 'No',
+          type: 'warning'
+        }).then(() => {
+        //to do 
+          console.log('studentid', this.userId, 'assignment_id', data.assignment_id,
+                'token_count', data.token_required,)
+          this.$http({
+          url: this.$http.adornUrl('/token/cancel_token/' + this.userId ),
+          method: 'delete',
+          data: this.$http.adornData({
+                'assignment_id': data.assignment_id,
+                'token_count': data.token_required,
+              })
+        }).then((response)=>{
+          // console.log(response.data)
+          if (response.data.assignment_id != "failed")
+          {
+            data.status = "submitted"
+          // this.$set(this.tableData,index,row)
+          // console.log(data.token_required)
+           this.tokenNumber = this.tokenNumber - data.token_required,
+           window.open('https:\\canvas.instructure.com/courses/3737737/assignments/'+ data.resubmission_id, '_blank'),
+           this.$message({
+            type: 'success',
+            message: 'get a resubmission link!'
+          })
+          }
+          else{
+            this.$message({
+            type: 'info',
+            message: response.data.message
+          });     
+          }
+        })
+      }).catch((e) => {
+          console.log(e)
+          this.$message({
+            type: 'info',
+            message: 'Cancel'
+          });          
+        });
+    },
       getAssignmentStatus() {
         this.$http({
           url: this.$http.adornUrl('/token/assignment_status/' + this.$store.state.user.studentID),
